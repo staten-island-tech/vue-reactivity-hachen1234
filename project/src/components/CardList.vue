aa3e762db9c3ed4d3f13363d7ad8795d394203df
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-02-09 10:14:29 -0500

    1

--- project/src/components/CardList.vue
+++ project/src/components/CardList.vue
@@ -2,6 +2,39 @@
   <div></div>
 </template>
 
-<script setup></script>
+<script setup>
+const card = [
+  {
+    name: "Victorian Clasped Hands Seal",
+    cost: "$960.00 ",
+    img: "./img/img1.webp",
+    used: true,
+  },
+  {
+    name: "19th Wonderful Antique Vintage Original Bronze Hand Statue Wax Seal C.",
+    cost: "$499.99 ",
+    img: "./img/img2.webp",
+    used: true,
+  },
+  {
+    name: "Victorian 18ct Gold Seal with Carnelian Base",
+    cost: "$600",
+    img: "./img/img3.jpg",
+    used: true,
+  },
+  {
+    name: " CRASPIRE Wax Seal Stamp",
+    cost: "$3.88",
+    img: "./img/img4.jpg",
+    used: false,
+  },
+  {
+    name: " 18th Century Noble Coat of Arms seal with original shagreen case ",
+    cost: "$2,450.00 ",
+    img: "./img/img5.avif",
+    used: false,
+  },
+];
+</script>
 
 <style scoped></style>
