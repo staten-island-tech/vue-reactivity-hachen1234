7d109755237b9113feb90a8437b7f0c962d98690
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-02-12 10:14:21 -0500

    1

--- project/src/components/CardList.vue
+++ project/src/components/CardList.vue
@@ -23,16 +23,40 @@ const card = [
     used: true,
   },
   {
-    name: " CRASPIRE Wax Seal Stamp",
+    name: "CRASPIRE Wax Seal Stamp",
     cost: "$3.88",
     img: "./img/img4.jpg",
     used: false,
   },
   {
-    name: " 18th Century Noble Coat of Arms seal with original shagreen case ",
-    cost: "$2,450.00 ",
+    name: "18th Century Noble Coat of Arms seal with original shagreen case ",
+    cost: "$2,450.00",
     img: "./img/img5.avif",
+    used: true,
+  },
+  {
+    name: "Large Size-Custom Wax Seal Stamp ",
+    cost: "$33.57",
+    img: "./img/img6.avif",
     used: false,
+  },
+  {
+    name: "desk stamp wax seal Staffordshire Hand Painted UK Pottery",
+    cost: "$200.39",
+    img: "./img/img7.webp",
+    used: false,
+  },
+  {
+    name: "Antique Victorian Wax Seal Stamp for Letter Sealing Wax with Carnelian Handle and Brass Flower Impress",
+    cost: "$165.00 ",
+    img: "./img/img8.webp",
+    used: false,
+  },
+  {
+    name: "Antique Seal, French, Napoleon III, Palais Royale MOP Hand Disc Fede",
+    cost: "$855.94  ",
+    img: "./img/img0.webp",
+    used: true,
   },
 ];
 </script>
