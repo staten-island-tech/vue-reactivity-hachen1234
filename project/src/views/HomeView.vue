755c5145bf1567974be19a86a55d5aa7bf5c1e88
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-02-16 09:53:41 -0500

    1

--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <h1>{{ card.name }}</h1>
+  </div>
 </template>
 
 <script>
+const props = defineProps({
+  card: object,
+});
+import { objectToString } from "@vue/shared";
 import card from "../components/CardList.vue";
 console.log(card);
 </script>
